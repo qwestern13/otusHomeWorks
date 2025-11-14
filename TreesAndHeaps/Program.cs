f38ebd6eb5e9a467d110740fee7d42b3ba85b47e
tiunov <qwestern13@yandex.ru>
2025-11-14 16:04:22 +0500

    added insert function

--- TreesAndHeaps/Program.cs
+++ TreesAndHeaps/Program.cs
@@ -1,3 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace TreesAndHeaps;
 
-Console.WriteLine("Hello, World!");
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+    }
+
+    static EmployeeTree Insert(EmployeeTree node, string name, int salary)
+    {
+        if (node == null)
+        {
+            return new EmployeeTree(name, salary);
+        }
+
+        if (salary < node.Salary)
+        {
+            node.Left = Insert(node.Left, name, salary);
+        }
+
+        else
+        {
+            node.Right = Insert(node.Right, name, salary);
+        } 
+        
+        return node;
+    }
+}
