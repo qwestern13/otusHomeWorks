b2005df6837d4f015c0fed2f60569e3e4393aebf
tiunov <qwestern13@yandex.ru>
2025-11-14 15:38:34 +0500

    first commit

---
+++ TreesAndHeaps/EmployeeTree.cs
@@ -0,0 +1,16 @@
+﻿namespace TreesAndHeaps;
+
+public class EmployeeTree
+{
+    public int Salary { get; set; }
+    public string Name { get; set; }
+    public EmployeeTree Left { get; set; }
+    public EmployeeTree Right { get; set; }
+    public EmployeeTree (string name, int salary)
+    {
+        Salary = salary;
+        Name = name;
+        Left = null;
+        Right = null;
+    }
+}
